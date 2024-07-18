570a38ea668c78f53c965d465d27abd466a9c960
Gustavo <gustavohdlima@hotmail.com>
2024-07-18 15:51:50 -0300

    feat: montando visual e funcionalidades de listagem e filtro de projetos da Home

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -1,11 +1,81 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from '../../../components/header/header.component';
+import {
+  ChangeDetectorRef,
+  Component,
+  effect,
+  OnInit,
+  signal,
+  WritableSignal,
+} from '@angular/core';
+import { StoreService } from 'app/services/store.service';
+import { Projects, UserDataType } from 'app/types/user-data-type';
+import { MatInputModule } from '@angular/material/input';
+import { AutocompleteChipsFormComponent } from 'app/components/autocomplete-chips-form/autocomplete-chips-form.component';
+import { TagType } from 'app/types/tag-type';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'environments/environment.dev';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { MatChipsModule } from '@angular/material/chips';
+import { AuthService } from 'app/modules/auth/auth.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    AutocompleteChipsFormComponent,
+    MatCardModule,
+    MatButtonModule,
+    MatChipsModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  userData = signal<UserDataType>(this.storeService.getCurrentUser());
+  user_avatar = signal<string>(this.userData()?.avatarUrl ?? 'assets/user_icon_2.png');
+  myProjects = signal(this.userData()?.projects ?? []);
+
+  tags = signal<TagType[]>([]);
+
+  constructor(
+    private httpClient: HttpClient,
+    private storeService: StoreService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    const response = this.httpClient.get(`${environment.baseUrl}/tags`, { withCredentials: true });
+    response.subscribe({
+      next: (res) => {
+        const allTags: TagType[] = res.valueOf() as TagType[];
+        this.tags.set(allTags);
+      },
+    });
+    this.authService.me().subscribe({
+      next: (res) => {
+        this.userData.set(res);
+        this.myProjects.set(this.userData()?.projects ?? []);
+      },
+    });
+  }
+
+  filterTags(newFilteredTags: WritableSignal<string[]>) {
+    // this.filteredtags.set(this.tags().filter((tag) => newFilteredTags().includes(tag.tagName)));
+
+    const filteredProjects: Projects[] = [];
+    newFilteredTags().forEach((t) => {
+      const actualFilteredProjects = this.userData()?.projects?.filter(
+        (p) =>
+          p.tags.map((el) => el.tagName).includes(t) &&
+          !filteredProjects?.some((i) => i.id === p.id)
+      );
+      if (actualFilteredProjects?.length) filteredProjects.push(...actualFilteredProjects);
+    });
+
+    if (newFilteredTags().length === 0) this.myProjects.set(this.userData()?.projects ?? []);
+    else this.myProjects.set(filteredProjects);
+  }
+}
